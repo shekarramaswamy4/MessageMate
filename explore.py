825ac66182dfbb28061e9502443812ec56e01a8f
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-10-16 14:52:59 -0700

    get chat history per number

--- explore.py
+++ explore.py
@@ -2,40 +2,45 @@ import sqlite3
 import pandas as pd
 from utils import format_tel
 
+address_books = [
+    "/Users/shekarramaswamy/Library/ApplicationSupport/AddressBook/Sources/58BDEBE3-DA9B-4BF3-A9CB-E5A17F4BC2CC/AddressBook-v22.abcddb",
+    "/Users/shekarramaswamy/Library/ApplicationSupport/AddressBook/Sources/1675CD53-E9F2-46BF-9DE1-12EF837D05BB/AddressBook-v22.abcddb"
+]
 # Connect to contacts
-cc = sqlite3.connect("/Users/shekarramaswamy/Library/ApplicationSupport/AddressBook/Sources/58BDEBE3-DA9B-4BF3-A9CB-E5A17F4BC2CC/AddressBook-v22.abcddb")
 # tables = pd.read_sql_query("select name from sqlite_master where type='table'", cc)
 # print(tables)
-cur = cc.cursor()
 
-# Gives name, ID
 id_to_name = {}
-cur.execute("select ZSORTINGFIRSTNAME, Z_PK from ZABCDRECORD order by Z_PK asc")
-for row in cur.fetchall():
-    if row[0] is not None and row[1] is not None:
-        # TODO: clean up first name
-        id_to_name[row[1]] = row[0]
-
-# Phone number, ID
 num_to_name = {}
-cur.execute("select ZFULLNUMBER, ZOWNER from ZABCDPHONENUMBER order by ZOWNER asc")
-for row in cur.fetchall():
-    if row[0] is not None and row[1] is not None:
-        if row[1] in id_to_name:
-            formatted_num = format_tel(row[0])
-            if formatted_num is None:
-                # Log this
-                break
-            num_to_name[format_tel(row[0])] = id_to_name[row[1]]
-        else:
-            # Log this
-            pass
+for a in address_books:
+    cc = sqlite3.connect(a)
+    cur = cc.cursor()
 
-print(num_to_name)
+    # Name, ID
+    cur.execute("select ZSORTINGFIRSTNAME, Z_PK from ZABCDRECORD order by Z_PK asc")
+    for row in cur.fetchall():
+        if row[0] is not None and row[1] is not None:
+            # TODO: clean up first name
+            id_to_name[row[1]] = row[0]
+
+    # Phone number, ID
+    cur.execute("select ZFULLNUMBER, ZOWNER from ZABCDPHONENUMBER order by ZOWNER asc")
+    for row in cur.fetchall():
+        if row[0] is not None and row[1] is not None:
+            if row[1] in id_to_name:
+                formatted_num = format_tel(row[0])
+                if formatted_num is None:
+                    # Log this
+                    break
+                num_to_name[format_tel(row[0])] = id_to_name[row[1]]
+            else:
+                # Log this
+                pass
+
 # substitute username with your username
 conn = sqlite3.connect('/Users/shekarramaswamy/Library/Messages/chat.db')
 
-# connect to the database
+# Connect to chats database
 cur = conn.cursor()
 # get the names of the tables in the database
 # cur.execute("select name from sqlite_master where type = 'table'") 
@@ -52,7 +57,8 @@ print(cols)
 
 # From messages / chats / etc., I want to reconstruct the latest message history given the
 # chat_identifier
-messages = pd.read_sql_query("""SELECT
+num_to_messages = {}
+cur.execute("""SELECT
     datetime (message.date / 1000000000 + strftime ("%s", "2001-01-01"), "unixepoch", "localtime") AS message_date,
     message.text,
     chat.chat_identifier
@@ -61,9 +67,27 @@ FROM
     JOIN chat_message_join ON chat. "ROWID" = chat_message_join.chat_id
     JOIN message ON chat_message_join.message_id = message. "ROWID"
 ORDER BY
-    message_date DESC 
-LIMIT 10;""", conn)
-print(messages)
+    message_date DESC""")
+for row in cur.fetchall():
+    if row[2].startswith("chat") or "icloud" in row[2]: # skip group chat or icloud numbers
+        continue
+    formatted_num = format_tel(row[2])
+    if formatted_num in num_to_messages:
+        num_to_messages[formatted_num].append(row[1]) 
+    else:
+        num_to_messages[formatted_num] = [row[1]]
+
+    
+count = 0
+for k in num_to_messages:
+    if k in num_to_name:
+        # print(num_to_name[k])
+        pass
+    else:
+        print(k, num_to_messages[k])
+        count += 1
+        print(str(k) + " not found") # TODO: type k upstream
+print(count)
 
 ######
 
