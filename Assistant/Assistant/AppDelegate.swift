e5aa70f4611916fccd96b1d356146df1dc3a542b
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-11-01 22:33:34 -0700

    fix

--- Assistant/Assistant/AppDelegate.swift
+++ Assistant/Assistant/AppDelegate.swift
@@ -13,11 +13,39 @@ let suggestion = Suggestion()
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Check full disk access permission
         
         let data = data.getData()
         let suggestion = suggestion.makeSuggestions(cmh: data)
+        
+        let vc = ViewController()
+        
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = vc
+        self.popover = popover
+        
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
